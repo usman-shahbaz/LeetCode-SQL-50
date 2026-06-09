428ad21c75b630be74d2ea07cda660fdd6935927
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-09 23:56:24 +0500

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,2 +1,18 @@
+def twoSum(self, numbers, target):
 
+    start = 0
+    end = len(numbers) - 1
 
+    while start < end:
+
+        total = numbers[start] + numbers[end]
+
+        if total == target:
+            return [start + 1, end + 1]
+
+        elif total < target:
+            start += 1
+        else:
+            end -= 1
+
+    return [-1, -1]
