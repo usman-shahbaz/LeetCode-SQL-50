46f7bc3cf29d65b8d65d49aad5f5c068fa7ca486
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-09 23:54:04 +0500

    Update Is_Subsequence.py

--- Is_Subsequence.py
+++ Is_Subsequence.py
@@ -1,1 +1,13 @@
+def isSubsequence(s, t) -> bool:
+    m = len(s)
+    n = len(t)
 
+    if m == 0: return True
+    if m > n: return False
+    l = 0
+
+    for ch in t:
+        if l < len(s) and s[l] == ch:
+            l += 1
+
+    return l == len(s)
