8fff26390eedc9612c59a8b5b73ed27cfe7db926
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-09 23:59:51 +0500

    Update Remove_Element.py

--- Remove_Element.py
+++ Remove_Element.py
@@ -1,1 +1,13 @@
+def removeElement(self, nums: List[int], val: int) -> int:
 
+    n = len(nums)
+    k = 0
+
+    for i in range(n):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
+
+    
