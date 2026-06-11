eff718346dbc73e13d3c08650ae385c884bf4080
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-11 22:52:52 +0500

    Update Valid_palindrome.py

--- Valid_palindrome.py
+++ Valid_palindrome.py
@@ -1,1 +1,13 @@
+def isPalindrome(s):
+    s = ''.join(c.lower() for c in s if c.isalnum())
+    left = 0 
+    right = len(s) - 1
 
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+        
+    return True
+  
