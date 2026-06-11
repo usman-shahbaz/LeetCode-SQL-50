972ef48ec1c85838260c1a48d2ea2d8365b6dd8f
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-11 22:48:06 +0500

    Update and rename Two_Sum_Sorted_Array.py to two_sum_torted.py

---
+++ two_sum_torted.py
@@ -0,0 +1,15 @@
+
+def twoSum(numbers, target):
+
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        total = numbers[left] + numbers[right]
+
+        if total == target:
+            return [left + 1, right + 1]
+        elif total > target:
+            right -= 1
+        else:
+            left += 1
