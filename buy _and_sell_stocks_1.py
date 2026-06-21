de281bd2b4f465f03d3d335b13ab79b694f33d20
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-21 23:56:05 +0500

    Update buy _and_sell_stocks_1.py

--- buy _and_sell_stocks_1.py
+++ buy _and_sell_stocks_1.py
@@ -1,1 +1,13 @@
+def maxProfit(prices):
+    
+    minprice = float('inf')
+    profit = 0
 
+    for price in prices:
+        if price < minprice:
+            minprice = price
+        
+        p = price - minprice
+        profit = max(profit, p)
+
+    return profit
