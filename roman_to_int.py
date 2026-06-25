b91f2ef7734d27270a302ced6e541ebb40695881
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-06-25 23:56:20 +0500

    Create roman_to_int.py

---
+++ roman_to_int.py
@@ -0,0 +1,20 @@
+def romanToInt(self, s: str) -> int:
+    
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    n = len(s)
+
+    for i in range(n):
+        if roman[s[i]] < roman[s[i+1]]:
+            total += roman[s[i]]
+        else:
+            total -= roman[s[i]]
+    return total
