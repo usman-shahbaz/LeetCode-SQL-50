c8c4b07d4ce151c76df7a04176d9b45553c79929
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-07-08 23:56:47 +0500

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,23 +1,22 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+def isAnagram(s, t):
 
-        if len(t) != len(s):
-            return False
-
-        need = Counter(t)
-        w = Counter()
-        l = 0
-        n = len(s)
-
-        for r in range(n):
-            w[s[r]] += 1
-
-            if r-l+1 > len(s):
-                w[s[l]] -= 1
-                if w[s[l]] == 0:
-                    del w[s[l]]
-                l += 1
-
-            if w == need:
-                return True
+    if len(t) != len(s):
         return False
+
+    need = Counter(t)
+    w = Counter()
+    l = 0
+    n = len(s)
+
+    for r in range(n):
+        w[s[r]] += 1
+
+        if r-l+1 > len(s):
+            w[s[l]] -= 1
+            if w[s[l]] == 0:
+                del w[s[l]]
+            l += 1
+
+        if w == need:
+            return True
+    return False
